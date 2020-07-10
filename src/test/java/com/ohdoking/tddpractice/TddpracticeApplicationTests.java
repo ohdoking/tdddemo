96426a14130213c87a3dcfb46b8f0289bbe3550e
Dokeun Oh <dokeun.oh@car-ewallet.de>
2020-07-11 01:34:38 +0200

    init

---
+++ src/test/java/com/ohdoking/tddpractice/TddpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohdoking.tddpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
